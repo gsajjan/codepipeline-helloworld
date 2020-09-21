91092dc5d17dc180d996762bbdab17bc3d20fdef
gsajjan <sajjanarg@gmail.com>
2020-09-21 18:10:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require('Appikon');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Appikon' });
 });
 
 module.exports = router;
